cb2ef53ee62e051bb890714f4c4bb2bf7ff07c4e
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-13 01:30:24 +0530

    Modified the image url with correct url

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    console.log(blog);
+    //destructuring from blog
+    const {title, cover_img, author_img} = blog;
     return (
         <div>
-            
+            <img src={cover_img} alt={`Cover picture of the title ${title}`} />
+            <img src={author_img} alt={`Cover picture of the title ${title}`} />
+            <h2 className='text-3xl'>{title}</h2>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     //We need state to hold data
@@ -15,6 +16,10 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3 mx-auto">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                //map each blog to show dynamically
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
