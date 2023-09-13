05c785d5dbe9b0dc21709171f233842bb13c4ea3
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-13 11:38:55 +0530

    Style cover images and author images and resize into div

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,21 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
     //destructuring from blog
-    const {title, cover_img, author_img} = blog;
+    const {title, cover_img, author_img,reading_time,author} = blog;
     return (
         <div>
-            <img src={cover_img} alt={`Cover picture of the title ${title}`} />
-            <img src={author_img} alt={`Cover picture of the title ${title}`} />
+            <img className='w-full' src={cover_img} alt={`Cover picture of the title ${title}`} />
+            <div className='p-2 flex justify-between'>
+                <div>
+                    <img className='w-10 h-10 rounded-full' src={author_img} alt={`Cover picture of the title ${title}`} />
+                    <div>
+
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
             <h2 className='text-3xl'>{title}</h2>
         </div>
     );
